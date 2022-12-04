a416f9f2b83db88815f28ba4cbf493f8b2be6c26
nasim <nasim.bd@asa-international.com>
2022-12-04 17:23:56 +0600

    add 10.2

--- venv/Day10/day10.1.py
+++ venv/Day10/day10.1.py
@@ -3,7 +3,7 @@ def format_name(f_name, l_name):
     return "You didn't provide valid inputs."
   formated_f_name = f_name.title()
   formated_l_name = l_name.title()
-  f"Result: {formated_f_name} {formated_l_name}"
+  return f"Result: {formated_f_name} {formated_l_name}"
 
 #Storing output in a variable
 formatted_name = format_name(input("Your first name: "), input("Your last name: "))
