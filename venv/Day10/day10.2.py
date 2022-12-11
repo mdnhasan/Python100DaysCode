75d3da5b817b0f7b4fd2d35f34ae7f30c1676e02
nasim <nasim.bd@asa-international.com>
2022-12-11 17:12:09 +0600

    Practic Code

--- venv/Day10/day10.2.py
+++ venv/Day10/day10.2.py
@@ -2,17 +2,21 @@ def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year.")
+                return True
             else:
-                print("Not leap year.")
+                return False
         else:
-            print("Leap year.")
+            return True
     else:
-        print("Not leap year.")
+        return False
 
 
 def days_in_month():
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month ==2:
+        return 29
+    return month_days[month]
+
 
 
 # 🚨 Do NOT change any of the code below
