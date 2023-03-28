493557f011e91fee87b3f470cac7bbe693f1e874
nasim <nasimhasancst@gmail.com>
2023-03-28 15:50:38 +0600

    day12

--- venv/Day12/day12.2.py
+++ venv/Day12/day12.2.py
@@ -7,3 +7,53 @@
 # Track the number of turns remaining.
 # If they run out of turns, provide feedback to the player.
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+from random import randint
+from art import logo
+
+EASY_LEVEL=10 ##global variable
+HARD_LEVEL=5 ##global variable
+
+def check_answer(guess, answer,turns):
+    if guess>answer:
+        print("Too High")
+        return turns-1
+    elif guess<answer:
+        print("Too Low")
+        return turns-1
+    else:
+        print(f"You got it. The answer is {guess}")
+
+def set_difficulty():
+    level=input("Choose Difficulty: easy or hard>> ").lower()
+    if level=='easy':
+      return EASY_LEVEL
+    elif level=='hard':
+        return HARD_LEVEL
+    else:
+        print("Invalid Choiche")
+
+
+
+def game():
+    print(logo)
+    print("Welcome to the game")
+    print("Thinking a number between 1 to 100")
+
+    answer=randint(1,100)
+    print(f"The correct answer is {answer}")
+    turns=set_difficulty() ##function call
+
+
+    guess=0 ##global variable
+    while guess!=answer:
+        print(f"You have {turns} attempts ")
+
+        guess=int(input("Make a Guess:"))
+
+        turns=check_answer(guess, answer,turns) ##function call and added to turns
+        if turns==0: ### attept end
+            print("You Lose")
+            return ###use to stop function
+
+game()
--- venv/Day8/day8.2.py
+++ venv/Day8/day8.2.py
@@ -3,7 +3,7 @@
 def prime_checker(number):
     is_prime=True
     for i in range(2,number):
-        if number%i==0:
+        if number%i==1:
             is_prime=False
     if is_prime:
         print("Prime")
