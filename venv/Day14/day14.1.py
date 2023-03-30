ccacaa4626999cf6b97bc8da59009b9078d2fc76
nasim <nasimhasancst@gmail.com>
2023-03-30 14:10:50 +0600

    day14

---
+++ venv/Day14/day14.1.py
@@ -0,0 +1,17 @@
+### Display Art
+from art import logo
+from gamedata import data
+
+### Generate a random number from data
+from random import randint
+
+
+### Format the account Data
+
+
+### User Guess
+
+
+
+### check user guess right or wrong
+
