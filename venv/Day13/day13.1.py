8b866896e3fd903157b9b4d5e090616c6e112fd0
nasim <nasimhasancst@gmail.com>
2023-03-30 13:08:30 +0600

    day13.2

--- venv/Day13/day13.1.py
+++ venv/Day13/day13.1.py
@@ -1,6 +1,6 @@
 ############DEBUGGING#####################
 
-# # Describe Problem
+# Describe Problem
 # def my_function():
 #   for i in range(1, 20):
 #     if i == 20:
@@ -10,7 +10,7 @@
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
+# dice_num = randint(0, 5)
 # print(dice_imgs[dice_num])
 
 # # Play Computer
@@ -21,24 +21,25 @@
 #   print("You are a Gen Z.")
 
 # # Fix the Errors
-# age = input("How old are you?")
+# age = int(input("How old are you?"))
 # if age > 18:
-# print("You can drive at age {age}.")
+#   print(f"You can drive at age {age}.")
 
-# #Print is Your Friend
+#Print is Your Friend
 # pages = 0
 # word_per_page = 0
+#
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
 
-# #Use a Debugger
+#Use a Debugger
 # def mutate(a_list):
 #   b_list = []
 #   for item in a_list:
 #     new_item = item * 2
-#   b_list.append(new_item)
+#     b_list.append(new_item)
 #   print(b_list)
-
+#
 # mutate([1,2,3,5,8,13])
