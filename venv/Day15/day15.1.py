943348462bd40752256f9230c5e1cc322b2ecf81
nasim <nasimhasancst@gmail.com>
2023-04-04 16:02:40 +0600

    day15.1

--- venv/Day15/day15.1.py
+++ venv/Day15/day15.1.py
@@ -9,21 +9,33 @@ def is_resource_available(oder_ingredients):
             return False
     return True
 
+
+def make_coffe(drink_name, oder_ingredients):
+    for item in oder_ingredients:
+        resources[item]-=oder_ingredients[item]
+
 def process_coin():
     """Retrun Total from inserted coin"""
     print("Please Insert Coin: ")
-    total=int(input("How Many Quarters")) * 0.25
-    total += int(input("How Many dimes")) * 0.1
-    total += int(input("How Many nickels")) * 0.05
-    total += int(input("How Many pennies")) * 0.01
+    total=int(input("How Many Quarters: ")) * 0.25
+    total += int(input("How Many dimes: ")) * 0.1
+    total += int(input("How Many nickels: ")) * 0.05
+    total += int(input("How Many pennies: ")) * 0.01
+    print(total)
     return total
 
 
 def is_transaction_successful(money_received,cost_of_coffe):
     """Check enough money or not"""
     if money_received>cost_of_coffe:
+        change=round(money_received-cost_of_coffe, 2)
+        print(f"Here is ${change} change")
+        global profit
+        profit+=cost_of_coffe
         return True
-
+    else:
+        print("Sorry Need more money")
+        return False
 
 
 is_on =True
@@ -44,5 +56,8 @@ while is_on:
         drink=MENU[user_choice]
         if is_resource_available(drink['ingredients']):
             payment=process_coin()
+            if is_transaction_successful(payment,drink['cost']):
+                make_coffe(user_choice,drink['ingredients'])
+
 
 
